55c13cf4aa6cd989c22257d3b9d1063db6203510
Gustavo Firmino <gustavofirmino5011@gmail.com>
2025-04-08 17:01:17 -0300

    Sistema de Salão v1.0

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Salão</title>
+    <title>Sistema de Salão</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <style>
         body{
